7122e834f225279d0c5291d14b73969587ebf936
Klimentsii <59070287+klimentsii@users.noreply.github.com>
2021-09-09 09:34:26 +0300

    init: start space dynasty

---
+++ js/script.js
@@ -0,0 +1,78 @@
+let bg = document.querySelector('.body__bg');
+window.addEventListener('mousemove', function (e) {
+   let x = e.clientX / window.innerWidth;
+   let y = e.clientY / window.innerHeight;
+   bg.style.transform = 'translate(-' + x * 100 + 'px, -' + y * 100 + 'px)';
+});
+let earth__img_planet = document.querySelector('.earth__img_planet');
+window.addEventListener('mousemove', function (e) {
+   let x = e.clientX / window.innerWidth;
+   let y = e.clientY / window.innerHeight;
+   earth__img_planet.style.transform = 'translate(-' + x * 10 + 'px, -' + y * 10 + 'px)';
+});
+let earth__img_bg = document.querySelector('.earth__img_bg');
+window.addEventListener('mousemove', function (e) {
+   let x = e.clientX / window.innerWidth;
+   let y = e.clientY / window.innerHeight;
+   earth__img_bg.style.transform = 'translate(-' + x * 8 + 'px, -' + y * 8 + 'px)';
+});
+let moon__img_planet = document.querySelector('.moon__img_planet');
+window.addEventListener('mousemove', function (e) {
+   let x = e.clientX / window.innerWidth;
+   let y = e.clientY / window.innerHeight;
+   moon__img_planet.style.transform = 'translate(-' + x * 10 + 'px, -' + y * 10 + 'px)';
+});
+let moon__img_bg = document.querySelector('.moon__img_bg');
+window.addEventListener('mousemove', function (e) {
+   let x = e.clientX / window.innerWidth;
+   let y = e.clientY / window.innerHeight;
+   moon__img_bg.style.transform = 'translate(-' + x * 8 + 'px, -' + y * 8 + 'px)';
+});
+let mars__img_planet = document.querySelector('.mars__img_planet');
+window.addEventListener('mousemove', function (e) {
+   let x = e.clientX / window.innerWidth;
+   let y = e.clientY / window.innerHeight;
+   mars__img_planet.style.transform = 'translate(-' + x * 10 + 'px, -' + y * 10 + 'px)';
+});
+let mars__img_bg = document.querySelector('.mars__img_bg');
+window.addEventListener('mousemove', function (e) {
+   let x = e.clientX / window.innerWidth;
+   let y = e.clientY / window.innerHeight;
+   mars__img_bg.style.transform = 'translate(-' + x * 8 + 'px, -' + y * 8 + 'px)';
+});
+let void__img_bg = document.querySelector('.void__img_bg');
+window.addEventListener('mousemove', function (e) {
+   let x = e.clientX / window.innerWidth;
+   let y = e.clientY / window.innerHeight;
+   void__img_bg.style.transform = 'translate(-' + x * 8 + 'px, -' + y * 8 + 'px)';
+});
+let body = document.querySelector('.body__column');
+window.addEventListener('mousemove', function (e) {
+   let x = e.clientX / window.innerWidth;
+   let y = e.clientY / window.innerHeight;
+   body.style.transform = 'translate(-' + x * 8 + 'px, -' + y * 8 + 'px)';
+});
+let earth__title = document.querySelector('.earth__title');
+window.addEventListener('mousemove', function (e) {
+   let x = e.clientX / window.innerWidth;
+   let y = e.clientY / window.innerHeight;
+   earth__title.style.transform = 'translate(-' + x * 8 + 'px, -' + y * 8 + 'px)';
+});
+let moon__title = document.querySelector('.moon__title');
+window.addEventListener('mousemove', function (e) {
+   let x = e.clientX / window.innerWidth;
+   let y = e.clientY / window.innerHeight;
+   moon__title.style.transform = 'translate(-' + x * 8 + 'px, -' + y * 8 + 'px)';
+});
+let mars__title = document.querySelector('.mars__title');
+window.addEventListener('mousemove', function (e) {
+   let x = e.clientX / window.innerWidth;
+   let y = e.clientY / window.innerHeight;
+   mars__title.style.transform = 'translate(-' + x * 8 + 'px, -' + y * 8 + 'px)';
+});
+let void__title = document.querySelector('.void__title');
+window.addEventListener('mousemove', function (e) {
+   let x = e.clientX / window.innerWidth;
+   let y = e.clientY / window.innerHeight;
+   void__title.style.transform = 'translate(-' + x * 8 + 'px, -' + y * 8 + 'px)';
+});
